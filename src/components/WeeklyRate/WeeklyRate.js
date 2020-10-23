d9d382d5bcb3324b900723e0222d01f30e8f17b2
samirbiswas <samir99biswas@gmail.com>
2020-10-23 11:54:21 +0600

    toggle button added

--- src/components/WeeklyRate/WeeklyRate.js
+++ src/components/WeeklyRate/WeeklyRate.js
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from 'react';
 import './WeeklyRate.css'
 
 const WeeklyRate = () => {
+
+  //toggle start
   const [state, setState] = useState({
     checkedA: true,
     checkedB: true,
@@ -10,6 +12,8 @@ const WeeklyRate = () => {
   const handleChange = (event) => {
     setState({ ...state, [event.target.name]: event.target.checked });
   };
+
+//toggle end
 
 const [weeklyData, setWeeklyData] = useState({});
 useEffect(() => {
@@ -23,42 +27,39 @@ useEffect(() => {
   },[]);
   const [bodyData, setBodyData] = useState([]);
 
-  useEffect(() => {
-    fetch('https://us-central1-stremlind-app.cloudfunctions.net/api/hotel/rate-comparison')
-    .then(res=>res.json())
-    .then(result=>{
-        setBodyData(result.rateBody)
-    })
-
+    useEffect(() => {
+      fetch('https://us-central1-stremlind-app.cloudfunctions.net/api/hotel/rate-comparison')
+      .then(res=>res.json())
+      .then(result=>{
+          setBodyData(result.rateBody)
+      })
 
   },[]);
 
     return (
         <div className='container'>
+
             <div className='mt-5 d-flex justify-content-between'>
-            <p>Hotel Rate Comparison <i className="arrow down"></i></p>
-            <div className='mb-3'>
-            <FormGroup row>
-            
-            <FormControlLabel
-              control={
-              <Switch
-                  checked={state.checkedB}
-                  onChange={handleChange}
-                  name="checkedB"
-                  color="primary"
-              />
-            }
-        label="Weekly Data"
-      />
-     
-    </FormGroup>
-            
+                <p>Hotel Rate Comparison <i className="arrow down"></i></p>
+                  <div className='mb-3'>
+                    <FormGroup row>
+                    
+                      <FormControlLabel
+                        control={
+                          <Switch
+                            checked={state.checkedB}
+                            onChange={handleChange}
+                            name="checkedB"
+                            color="primary"
+                          />}
+                          
+                        label="Weekly Data"
+                    />
+                  </FormGroup>
+              </div>
             </div>
 
-            </div>
-
-            <table className="table table-hover">
+<table className="table table-hover">
   <thead className='design'>
       <tr>
           <th className='text-black-50 ' scope="col">{weeklyData[0]}</th>
@@ -73,12 +74,12 @@ useEffect(() => {
   </thead>
 
   <tbody >
-  {bodyData.map(or => {
+      {bodyData.map(or => {
             const keys = Object.keys(or).slice(1)
-            return (
-              <tr  >
-                <td >{or.name}</td>
-                {keys.map(day => <td  >${ or[day]?.otherPrice} <span style= {{color:'red',fontSize:'10px'}} >${or[day]?.compare}</span> </td>)}
+              return (
+                <tr>
+                  <td >{or.name}</td>
+                  {keys.map(day => <td  >${ or[day]?.otherPrice} <span style= {{color:'red',fontSize:'10px'}} >${or[day]?.compare}</span> </td>)}
               </tr>
             )
           }
@@ -86,12 +87,8 @@ useEffect(() => {
           
   </tbody>
 </table>
-           <div>
-           
-           </div>
         </div>
     );
 };
-
 
 export default WeeklyRate;
